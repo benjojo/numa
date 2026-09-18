1058a779f4341db3ec45a72cd00a4b11a61832de
Ben Cartwright-Cox <ben@benjojo.co.uk>
2026-09-18 22:49:45 +0100

    go 1.27 fixes, Abolish "fast way" for GetCPUAndNode
    because at least in my situations, this does not need to be
    super streamlined and the previous setup required us to do stuff
    that was just a pain for future versions of go.
    
    Instead, we will just bang out a raw syscall, I am sure this
    will last longer than what we had before

--- numa_linux_amd64.go
+++ numa_linux_amd64.go
@@ -1,22 +1,39 @@
 package numa
 
 import (
+	"syscall"
+	"unsafe"
+
 	"github.com/intel-go/cpuid"
 )
 
 var fastway = cpuid.HasFeature(cpuid.RDTSCP)
 
-func getcpu()
+//go:noescape
+func getCPUAndNodeRDTSCP() (cpu int, node int)
 
 // GetCPUAndNode returns the node id and cpu id which current caller running on.
 // https://man7.org/linux/man-pages/man2/getcpu.2.html
 //
 // equal:
 //
-// if fastway {
-// 	call RDTSCP
-//  The linux kernel will fill the node cpu id in the private data of each cpu.
-//  arch/x86/kernel/vsyscall_64.c@vsyscall_set_cpu
-// }
-// call vdsoGetCPU
-func GetCPUAndNode() (cpu int, node int)
+//	if fastway {
+//		call RDTSCP
+//	 The linux kernel will fill the node cpu id in the private data of each cpu.
+//	 arch/x86/kernel/vsyscall_64.c@vsyscall_set_cpu
+//	}
+func GetCPUAndNode() (cpu int, node int) {
+	if fastway {
+		return getCPUAndNodeRDTSCP()
+	}
+	const sysGetCPU = 309
+	_, _, errno := syscall.RawSyscall(sysGetCPU,
+		uintptr(unsafe.Pointer(&cpu)),
+		uintptr(unsafe.Pointer(&node)),
+		0)
+	if errno != 0 {
+		cpu = 0
+		node = 0
+	}
+	return
+}
